bd826b648aa1056f9769b4ce04569fd81788b058
Darenn Keller <keller.darenn@gmail.com>
2016-12-02 14:38:05 +0100

    shape rotate

--- shape_rotate.cpp
+++ shape_rotate.cpp
@@ -4,5 +4,6 @@
 #define NDEBUG 1
 
 bool Shape_Rotate::contains(double const x, double const y) const {
-  return true;
+  return sh->contains(x * cos(c_alpha) - y * (sin(s_alpha)),
+                      x * sin(s_alpha) - y * (cos(c_alpha)));
 }
